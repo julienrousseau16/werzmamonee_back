69618293cd16dff7d9a941bd940aa9b001f8269d
Julien Rousseau <julienrousseau16@gmail.com>
2021-01-04 17:20:22 +0100

    new temporary route created to update the bank account current position

--- index.js
+++ index.js
@@ -21,6 +21,32 @@ app.get('/expense', (req, res) => {
   })
 })
 
+app.get('/admin', (req, res) => {
+  connection.query('SELECT * FROM user WHERE id = 1', (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Error while getting admin data')
+    }
+    res.json(results[0])
+  })
+})
+
+app.put('/admin', (req, res) => {
+  const formData = req.body
+  connection.query('UPDATE user SET ? WHERE id = 1', formData, (err, results) => {
+    if (err) {
+      res.status(500).send('Error while changing user data')
+    }
+    connection.query('SELECT * FROM user WHERE id = 1', (err, update) => {
+      if (err) {
+        throw new Error('Oopsy')
+      }
+      console.log(update)
+      res.json(update)
+    })
+  })
+})
+
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error('Something bad happened...')
