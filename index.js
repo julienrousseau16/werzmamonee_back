84c3480e45c04617ead3d0b51413a6d979017bcf
Julien Rousseau <julienrousseau16@gmail.com>
2021-01-04 10:40:36 +0100

    first route to expense table is created and effective

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 // const { check, validationResult } = require('express-validator')
 const connection = require('./db-config')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
+
+app.get('/expense', (req, res) => {
+  connection.query('SELECT * FROM expense', (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Error while getting expenses')
+    }
+    res.json(results)
+  })
+})
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
