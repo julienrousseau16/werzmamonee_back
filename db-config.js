9e53982cca415d1f2cd9500afd6e19f959f6378b
Julien Rousseau <julienrousseau16@gmail.com>
2021-01-13 10:30:00 +0100

    CLEARDB_DATABASE_URL env variable use for DB settings

--- db-config.js
+++ db-config.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
+const dbSettings = typeof process.env.CLEARDB_DATABASE_URL === 'string' ?
+  process.env.CLEARDB_DATABASE_URL
+  : {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+
+const connection = mysql.createConnection(dbSettings)
 
 module.exports = connection
